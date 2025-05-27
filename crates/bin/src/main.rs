eeb4e96365a45f9898f567077baf13c5143d65d6
中宮 ちひろ <ciel_katana@outlook.com>
2025-05-27 15:11:33 +0800

    add camera rendering

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -10,13 +10,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Image
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
-
     let image_height = match (image_width as f32 / aspect_ratio) as i32 {
         n if n < 1 => 1,
         n => n,
     };
 
     // Camera
+    // Right-handed coordinates.
     let focal_length = 1.0;
     let viewport_height = 2.0;
     let viewport_width =
@@ -61,8 +61,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn ray_color(_r: &Ray) -> Color {
-    let unit_direction = _r.direction().unit_vector();
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().unit_vector();
     let a = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
 }
